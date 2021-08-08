4b29c519834895010897f94222ee0c65e001775a
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2021-08-08 11:44:56 +0800

    feat: support for armv6

--- compile-all.sh
+++ compile-all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ./dockcross/dockcross-linux-armv5 bash -c './compile.sh Linux arm'
+./dockcross/dockcross-linux-armv6-lts bash -c './compile.sh Linux armv6'
 ./dockcross/dockcross-linux-armv7 bash -c './compile.sh Linux armv7'
 ./dockcross/dockcross-linux-arm64 bash -c './compile.sh Linux aarch64'
 ./dockcross/dockcross-linux-x86 bash -c './compile.sh Linux x86'
