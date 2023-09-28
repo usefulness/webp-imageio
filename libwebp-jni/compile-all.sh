7c16c9a33b342413422f955f2a880346cee5effc
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2023-09-28 10:38:36 +0200

    Make room for docker images

--- libwebp-jni/compile-all.sh
+++ libwebp-jni/compile-all.sh
@@ -4,6 +4,7 @@ set -ex -o pipefail
 # Ensure clean build environments
 rm -rf build
 
+df -H
 ./dockcross/dockcross-linux-armv5 bash -c './compile.sh Linux arm'
 ./dockcross/dockcross-linux-armv6-lts bash -c './compile.sh Linux armv6'
 ./dockcross/dockcross-linux-armv7-lts bash -c './compile.sh Linux armv7'
@@ -12,11 +13,15 @@ rm -rf build
 ./dockcross/dockcross-manylinux-x64 bash -c './compile.sh Linux x86_64'
 ./dockcross/dockcross-linux-ppc64le bash -c './compile.sh Linux ppc64'
 
+df -H
 ./dockcross/dockcross-windows-static-x86 bash -c './compile.sh Windows x86'
 ./dockcross/dockcross-windows-static-x64 bash -c './compile.sh Windows x86_64'
 
+df -H
 docker run --rm -v $(pwd):/workdir -e CROSS_TRIPLE=x86_64-apple-darwin gotson/crossbuild ./compile.sh Mac x86_64 /workdir/multiarch-darwin.cmake
 docker run --rm -v $(pwd):/workdir -e CROSS_TRIPLE=aarch64-apple-darwin gotson/crossbuild ./compile.sh Mac aarch64 /workdir/multiarch-darwin.cmake
+
+df -H
 
 # Ensure clean target
 rm -r ../webp-imageio/src/main/resources/native
